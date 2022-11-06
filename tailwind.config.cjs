e5ead5fa51e6d2a6ebe97b4a3e2ffe7761cfce22
Koen Gommers <koen@koengommers.nl>
2022-11-06 18:20:40 +0100

    Setup tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          50: "#FFFFFF",
+          100: "#EEEEEE",
+          500: "#484848",
+          600: "#363636",
+          700: "#282828",
+          800: "#242424",
+          900: "#202020",
+        },
+        red: {
+          500: "#DE4C4A",
+          600: "#C53828",
+        },
+      },
+    },
   },
   plugins: [],
 };
